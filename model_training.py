7928ddbffebe33aa80e8f8d11d940c64d2401210
Varun Kuntal <varunkuntal@yahoo.in>
2022-09-03 23:43:23 +0530

    Model Registry Added, Register & Load

---
+++ model_training.py
@@ -0,0 +1,176 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import warnings
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
+warnings.simplefilter('ignore', ConvergenceWarning)
+warnings.filterwarnings('ignore', 'statsmodels.tsa.arima_model.ARMA', FutureWarning)
+warnings.filterwarnings('ignore', 'statsmodels.tsa.arima_model.ARIMA', FutureWarning)
+
+import numpy as np
+import yfinance as yf
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from datetime import date
+
+from itertools import product
+import mlflow
+from mlflow.entities import ViewType
+from mlflow.tracking import MlflowClient
+import mlflow.pyfunc
+
+MLFLOW_TRACKING_URI = 'http://127.0.0.1:5000'
+EXPERIMENT_NAME = "arima-ethusdt"
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment(EXPERIMENT_NAME)
+
+# Download cryptocurrency data (2017 - 2022)
+def read_data(ticker='ETH-USD'):
+    today = date.today()
+    datetoday = today.strftime("%B %d, %Y")
+    print(f"\nDownloading the dataset for {ticker} on {datetoday} ...\n\n")
+    df = yf.download(ticker)
+    df = df.reset_index()
+    df['Timestamp'] = df.Date.apply(lambda x: pd.Timestamp(x).timestamp())
+    df = df.drop(labels=['Date', 'Adj Close'], axis=1)
+    df = df[['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    df['Timestamp'] = df['Timestamp'].apply(lambda x: int(str(x)[:-2] + "000"))
+    
+    df.to_csv("data/" + ticker+"_"+today.strftime("%d_%m_%Y")+".csv", index=False)
+
+    # 10% data to test
+    training_data, testing_data = train_test_split(df, test_size=0.1, shuffle=False)
+    training_data, testing_data = list(training_data['Close']), list(testing_data['Close'])
+    return df, training_data, testing_data
+
+def mape(model_predictions, testing_data):
+    return np.mean(np.abs(np.array(model_predictions) - np.array(testing_data)) / np.abs(testing_data))
+
+def train_model_search():
+    df, training_data, testing_data = read_data()
+    
+
+    # Using GRID Search instead of Randomized Hyperopt as number of prameters are lesser
+    ps = range(2, 5)
+    qs = range(0, 1)
+    d=1
+    parameters = product(ps, qs)
+    parameters_list = list(parameters)
+
+    for param in parameters_list:
+        model_predictions = []
+        with mlflow.start_run(run_name='arima_param'):
+            mlflow.set_tag("model", "ARIMA")
+            print(f"p & q values are: {param[0]}, {param[1]}")
+            mlflow.log_param('param-ps', param[0])
+            mlflow.log_param('param-qs', param[1])
+            n_test_obs = len(testing_data)
+
+            try:
+                for i in range(n_test_obs):
+                    model = ARIMA(endog=training_data, order = (param[0], d, param[1]))
+                    model_fit = model.fit()
+                    output = model_fit.forecast()
+                    yhat = output[0]
+                    model_predictions.append(yhat)
+                    actual_test_value = testing_data[i]
+                    # Rolling prediction one day ahead and appending result to training + reiterate
+                    training_data.append(actual_test_value)
+
+                print(f"Model Predictions, Testing Data = {len(model_predictions)}, {len(testing_data)}")
+                mape_val = mape(model_predictions, testing_data)
+
+                mlflow.log_metric("AIC", model_fit.aic)
+                mlflow.log_metric("BIC", model_fit.bic)
+                mlflow.log_metric("mape", mape_val)
+                mlflow.pmdarima.log_model(model_fit, artifact_path="ARIMA_ETHUSD")
+
+            except Exception as e:
+                print(e)
+                print("Logging AIC, BIC, mape as inf")
+                mlflow.log_metric("AIC", float('inf'))
+                mlflow.log_metric("BIC", float('inf'))
+                mlflow.log_metric("mape", float('inf'))
+
+    return 
+
+def register_model():
+
+    client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+    experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
+
+    runs = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.mape ASC"]
+    )
+
+    if runs:
+        for run in runs:
+            print(f"run id: {run.info.run_id}, mape: {run.data.metrics['mape']:.4f}")
+
+            artifact_path = "ARIMA_ETHUSD"
+            model_name = "ARIMA-FORECASTING-MODEL"
+
+            model_uri = f'runs:/{run.info.run_id}/{artifact_path}'
+
+            model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
+
+            client.transition_model_version_stage(
+            name=model_details.name,
+            version=model_details.version,
+            stage='staging',
+            )
+
+    else:
+        print(f"No runs found in the experiment {EXPERIMENT_NAME}")
+        
+    return
+    
+
+def train():
+    model_predictions = []
+    
+    df, training_data, testing_data = read_data()
+    n_test_obs = len(testing_data)
+
+    print("\nTraining the model...\n")
+    for i in range(n_test_obs):
+        model = ARIMA(training_data, order = (4,1,0))
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast()
+        yhat = list(output[0])[0]
+        model_predictions.append(yhat)
+        actual_test_value = testing_data[i]
+        # Rolling Training by predicting one day ahead and appending to 
+        training_data.append(actual_test_value)
+
+    mape_val = mape(model_predictions, testing_data)
+
+    return model_fit, mape_val
+
+def main():
+    # df, training_data, testing_data = read_data()
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("arima-ethusdt")
+    print(f"Searching Best Model ...")
+    train_model_search()
+    print(f"Registering Model.")
+    register_model()
+    print("Done.")
+    # model, mape_val = train()
+    #print(f"MAPE = {mape_val}\n\nSaving the model now...")
+    #model.save('model/model.pkl')
+    #print("Saved.")
+
+    #return mape_val
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
---
+++ predict.py
@@ -0,0 +1,79 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from statsmodels.tsa.arima_model import ARIMAResults
+from flask import Flask, request, jsonify, render_template
+from datetime import datetime, timedelta
+import datetime
+import requests
+import pandas as pd
+import json
+import os
+import jinja2
+env = jinja2.Environment()
+env.globals.update(zip=zip)
+import mlflow
+from mlflow.tracking import MlflowClient
+
+MLFLOW_TRACKING_URI = "http://127.0.0.1:5000"
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+
+def load_model():
+    # model = ARIMAResults.load('model/model.pkl')
+    artifact_path = "ARIMA_ETHUSD"
+    model_name = "ARIMA-FORECASTING-MODEL"
+    model_version = 1
+    
+    model = mlflow.pmdarima.load_model(model_uri=f"models:/{model_name}/{model_version}")
+    return model
+
+def newest(path):
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
+
+def prediction(timestamps = 1):
+    model = load_model()
+    return list(model.forecast(timestamps))
+
+app = Flask('crypto-prediction')
+
+@app.route('/')
+def landing_page():
+    return render_template('index.html', datelist="", show_results="false")
+
+@app.route('/pipe', methods=["GET", "POST"])
+def pipe():
+
+    latest_file = newest(r"data")
+    df = pd.read_csv(latest_file)
+    df = list(map(list, df.itertuples(index=False)))
+    return {"res":df}
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict_endpoint():
+    
+    timestamp = request.form['timestamp']   
+    print(timestamp)
+    pred = prediction(timestamps = int(timestamp))
+
+    latest_file = newest(r"data")
+    
+    day = int(latest_file.split("_")[1])
+    month = int(latest_file.split("_")[2])
+    year = int(latest_file.split("_")[3].split(".")[0])
+
+    current = datetime.datetime(year, month, day)
+
+    datelist = []
+
+    for i in range(1, int(timestamp)+1):
+        datelist.append([pred[i-1], (current+timedelta(days=i)).date()])
+
+    if timestamp:
+        return render_template('index.html', datelist = datelist, show_results="true")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
